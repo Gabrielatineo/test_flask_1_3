401dce950bad95cc55f3f4288d2824119a8985db
Gabrielatineo <131632382+Gabrielatineo@users.noreply.github.com>
2023-08-14 20:47:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask, request
 
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return {"payload":"welcome to my project"}
+
+if __name__ == "main":
+    app.run(debug=True)
